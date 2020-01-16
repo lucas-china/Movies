0c78be7179cd06bef882a9723e7a1cc799dd35b9
Lucas <lucasbrito_9@hotmail.com>
2020-01-16 01:12:14 -0300

    Show the details movie.

--- Movies/Controller/MovieDetailsViewController.swift
+++ Movies/Controller/MovieDetailsViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
     
+    var movie: Movie?
+    
     @IBOutlet weak var movieImageView: UIImageView!
     @IBOutlet weak var detailsMoviesTextView: UITextView!
     
-
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configeDetails()
     }
     
+    private func configeDetails() {
+       if let movie = movie {
+           
+           title = movie.title
+           
+           if let backdropPath = movie.backdropPath {
+               Util.createActivityIndicator(activityIndicator: activityIndicator, view: movieImageView)
+               
+               let url = URL(string: "\(urlBaseImageBig)\(backdropPath)")
+               movieImageView.sd_setImage(with: url) { (image, error, imageCache, url) in
+                   Util.stopActivityIndicator(activityIndicator: self.activityIndicator)
+               }
+           }
+          
+           if movie.overview!.isEmpty {
+               detailsMoviesTextView.text = "Sem resumo do filme."
+           } else {
+               detailsMoviesTextView.text = movie.overview
+           }
+           
+           detailsMoviesTextView.isEditable = false
+       }
+   }
 }
