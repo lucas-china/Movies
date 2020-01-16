ad08422dcb7fb252b80e7c70c67cdd747e6a7758
Lucas <lucasbrito_9@hotmail.com>
2020-01-16 01:11:49 -0300

    show the movies.

--- Movies/Controller/MoviesViewController.swift
+++ Movies/Controller/MoviesViewController.swift
@@ -10,11 +10,90 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     @IBOutlet weak var moviesCollectionView: UICollectionView!
-   
+   var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+   var moviesArray = [Movie]()
+   var page = 1
+   var totalPages = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initAplication()
     }
-   
+    
+    func initAplication() {
+        if Util.isConnectedToInternet() {
+            downloadMovies()
+        } else {
+            let mensagem = "Para usar o aplicativo, conecte com a internet."
+            let alert = UIAlertController(title: "Ops!", message: mensagem, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
+                action in
+                self.initAplication()
+            }))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func downloadMovies() {
+        Util.createActivityIndicator(activityIndicator: activityIndicator, view: self.view)
+        Movie.downloadMovies(page: page) { (isSucess, movies, totalPages) in
+            if isSucess {
+                self.moviesArray = movies
+                self.totalPages = totalPages
+                self.page += 1
+                self.moviesCollectionView.reloadData()
+                Util.stopActivityIndicator(activityIndicator: self.activityIndicator)
+            }
+        }
+    }
+}
+
+extension MoviesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return moviesArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath)
+        
+        if let cellMovie = cell as? MovieCollectionViewCell {
+            cellMovie.updateUI(movie: moviesArray[indexPath.row])
+            return cellMovie
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let viewController = storyboard?.instantiateViewController(withIdentifier: "moviesDetailsVC")
+        
+        if let movieDetailsVC = viewController as? MovieDetailsViewController {
+            movieDetailsVC.movie = moviesArray[indexPath.row]
+            navigationController?.pushViewController(movieDetailsVC, animated: true)
+        }
+    }
+}
+
+extension MoviesViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = view.frame.width/2.18
+        let height = view.frame.height/2.35
+        
+        let size = CGSize(width: width, height: height)
+        
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 10.0,
+                            left: 10.0,
+                            bottom: 10.0,
+                            right: 10.0)
+    }
 }
--- Movies/Views/Cells/MovieCollectionViewCell.swift
+++ Movies/Views/Cells/MovieCollectionViewCell.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
-
+import SDWebImage
 class MovieCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var folderView: UIView!
     @IBOutlet weak var movieImageView: UIImageView!
     @IBOutlet weak var nameMovieLabel: UILabel!
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
+    func updateUI(movie: Movie) {
+        nameMovieLabel.text = movie.title
+        
+        if let posterPath = movie.posterPath {
+            Util.createActivityIndicator(activityIndicator: activityIndicator, view: movieImageView)
+            
+            let url = URL(string: "\(urlBaseImage)\(posterPath)")
+            movieImageView.sd_setImage(with: url) { (image, error, imageCache, url) in
+                Util.stopActivityIndicator(activityIndicator: self.activityIndicator)
+            }
+        }
+        
+        setupFolderView()
+    }
+    
+    private func setupFolderView() {
+        folderView.layer.backgroundColor = UIColor.white.cgColor
+        folderView.layer.shadowColor = UIColor.white.cgColor
+        folderView.layer.shadowOpacity = 0.2
+        folderView.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        folderView.layer.shadowRadius = 4.0
+    }
 }
